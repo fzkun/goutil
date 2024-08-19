168aca972bbdfd20b73da7015572940c677a98b1
fzkun <412657308@qq.com>
2024-08-19 15:15:05 +0800

    增加slice方法

--- slice.go
+++ slice.go
@@ -1,12 +1,42 @@
 package goutil
 
 // SliceToMap 泛型函数，将一个包含键值对的切片转换为映射
-func SliceToMap[K comparable, V any](items []V, keyFunc func(V) K) map[K]V {
+func SliceToMap[K comparable, V any](list []V, keyFunc func(V) K) map[K]V {
 	dictionary := make(map[K]V)
 
-	for _, item := range items {
+	for _, item := range list {
 		dictionary[keyFunc(item)] = item
 	}
 
 	return dictionary
+}
+
+func SliceForEach[S ~[]E, E any](list S, f func(E)) {
+	for i := range list {
+		f(list[i])
+	}
+}
+
+// SliceToField 数组对象返回一个字段的数组
+func SliceToField[T any, F any](arr []T, field func(T) F) []F {
+	var result []F
+	for _, item := range arr {
+		result = append(result, field(item))
+	}
+	return result
+}
+
+// SliceRemoveDuplicates 定义一个泛型函数，用于去重
+func SliceRemoveDuplicates[T comparable](arr []T) []T {
+	seen := make(map[T]bool)
+	result := []T{}
+
+	for _, value := range arr {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+
+	return result
 }
--- slice_test.go
+++ slice_test.go
@@ -6,19 +6,37 @@ import (
 )
 
 func TestSliceToMap(t *testing.T) {
-	type TmpStruct struct {
-		Key   string
-		Value any
-	}
 	var (
-		list = []TmpStruct{
-			{"abc", 123},
-			{"abc2", 1234},
-			{"abc3", 12345},
-		}
+		list = []int{1, 2, 3}
 	)
-	toMap := SliceToMap(list, func(item TmpStruct) string {
-		return item.Key
+	toMap := SliceToMap(list, func(i int) int {
+		return i
 	})
 	fmt.Println(toMap)
+}
+
+func TestSliceToField(t *testing.T) {
+	// 定义一个示例结构体
+	type Person struct {
+		Name string
+		Age  int
+	}
+	// 示例数据
+	people := []Person{
+		{Name: "Alice", Age: 30},
+		{Name: "Bob", Age: 25},
+		{Name: "Charlie", Age: 35},
+	}
+
+	// 提取 Name 字段
+	names := SliceToField(people, func(p Person) string {
+		return p.Name
+	})
+	fmt.Println("Names:", names)
+
+	// 提取 Age 字段
+	ages := SliceToField(people, func(p Person) int {
+		return p.Age
+	})
+	fmt.Println("Ages:", ages)
 }
